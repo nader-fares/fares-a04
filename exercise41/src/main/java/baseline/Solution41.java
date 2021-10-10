58538d4a82280d6347fc968b559814d3cc37e5b1
nader-fares <59976904+nader-fares@users.noreply.github.com>
2021-10-10 18:19:52 -0400

    added pseudocode for exercise 41

---
+++ exercise41/src/main/java/baseline/Solution41.java
@@ -0,0 +1,24 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Nader Fares
+ */
+/*
+Create a program that reads in the following list of names from a file called `exercise41_input.txt` and sorts the list alphabetically:
+Then print the sorted list to a file called `exercise41_output.txt` that looks like the following example output.
+ */
+package baseline;
+
+import java.util.ArrayList;
+
+public class Solution41 {
+    public static void main(String[] args) {
+        //Declare arraylist to store names
+        ArrayList<String> names = new ArrayList<String>();
+
+        //read names from input file
+            //use loop to iterate through each line
+
+        //sort names
+        //write names onto output file
+    }
+}
