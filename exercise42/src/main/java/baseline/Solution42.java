4242186f167469fa11206df8eff2b5850abdda95
nader-fares <59976904+nader-fares@users.noreply.github.com>
2021-10-12 15:36:29 -0400

    exericse 42: added pseudocode

---
+++ exercise42/src/main/java/baseline/Solution42.java
@@ -0,0 +1,43 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Nader Fares
+ */
+/*
+Construct a program that reads in the following data file (you will need to create this data file yourself; name it `exercise42_input.txt`):
+Process the records and display the results formatted as a table, evenly spaced, as shown in the example output.
+Write your own code to parse the data. Don't use a CSV parser.
+ */
+
+package baseline;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution42 {
+    public static void main(String[] args) {
+        Solution42 app = new Solution42();
+
+        //declare list to store employees
+        List<String> employees = new ArrayList<>();
+
+        //declare lists to store last, first names and salaries
+        List<String> lastName = new ArrayList<>();
+        List<String> firstName = new ArrayList<>();
+        List<String> salary = new ArrayList<>();
+
+    }
+
+    //read names from input file
+    public void readFromFile() {
+        //add each employee to list
+    }
+
+    //write to output file
+    public void writeToFile() {
+    }
+
+    public void employeeParser() {
+        //separate each line
+        //store into separated lists
+    }
+}
