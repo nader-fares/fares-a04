351d0a4635a45a72baefada9ee5c5678730d42cf
nader-fares <59976904+nader-fares@users.noreply.github.com>
2021-10-14 23:31:05 -0400

    exercise 44: added pseudocode

---
+++ exercise44/src/main/java/baseline/Products.java
@@ -0,0 +1,8 @@
+package baseline;
+
+public class Products {
+    //product information
+    String name;
+    double price;
+    int quantity;
+}
---
+++ exercise44/src/main/java/baseline/Solution44.java
@@ -0,0 +1,22 @@
+package baseline;
+
+import java.util.List;
+
+public class Solution44 {
+    public static void main(String[] args) {
+        //declare list of product object
+        List<Products> products;
+
+        //prompt for product name and store string
+        String productName;
+
+        //search through list of product object names for input
+        //if found print out name, price and quantity
+        //else keep looping until found
+    }
+
+    public void productDeserialization() {
+        //read json file
+        //store information onto list of products objects
+    }
+}
