8919419a5d3e8444d2a5133b3eba72fbb2753bc0
nader-fares <59976904+nader-fares@users.noreply.github.com>
2021-10-15 01:47:13 -0400

    exercise 45: added pseudocode

---
+++ exercise45/src/main/java/baseline/Solution45.java
@@ -0,0 +1,24 @@
+package baseline;
+
+public class Solution45 {
+    public static void main(String[] args) {
+        //read from input file
+        //build output
+        //prompt for output file name
+        String outputFile;
+
+        //write output to file
+    }
+
+    //read from input file
+    public void readAndReplace() {
+        StringBuilder output;
+        //read file
+        //replace all instances of utilize with use
+        //return output
+    }
+
+    //write output to file
+    public void writeToFile() {
+    }
+}
